cf66377d75e68edd84d1ec7ee879e0f31145c6d5
Ritik Gupta <gritik1306@gmail.com>
2024-09-08 11:11:14 +0530

    index.js file changed

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-let expression=" ";
+let expression = "";
 
-function getOperation(button){
-	const op=button.getAttribute("data-op");
-	expression +=`${op}`;
-	document.getElementsByClassName("display").value=expression;
+function getOperation(button) {
+  const op = button.getAttribute("data-op");
+  expression += `${op}`;
+  document.getElementById("input1").value = expression;
 }
-function getNumber(button){
-	const number=button.innerText;
-	expression +=number;
-	document.getElementsByClassName("display").value=expression;
+function getNumber(button) {
+  const number = button.innerText;
+  expression += number;
+  document.getElementById("input1").value = expression;
 }
-function clearDisplay(){
-	expression="";
-	document.getElementById("input1").value="";
+function clearDisplay() {
+  expression = "";
+  document.getElementById("input1").value = "";
 }
-function runCalculator(){
-	const result=eval(expression);
-	document.getElementsByClassName("display").value=result;
-	expression=result.toString();
+function runCalculator() {
+  try {
+    const result = eval(expression);
+    document.getElementById("input1").value = result;
+    expression = result.toString();
+  } catch {
+    document.getElementById("input1").value = "Error";
+    expression = "";
+  }
 }
